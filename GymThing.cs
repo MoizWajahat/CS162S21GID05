41b4043ec4db78438631e48c75196ac8c0260da3
BlueRamus <moizwajahat3@gmail.com>
2021-06-19 16:26:00 +0500

    Added GymThing

---
+++ GymThing.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class GymThing
+    {
+        private List<Exercise> ExerList;
+        private List<WorkoutBase> WorkList;
+        private List<Machine> MachList;
+        private List<Item> ItemList;
+
+        //Getter Functions
+        public List<Exercise> ExerGet ()
+        {
+            return ExerList;
+        }
+        public List<WorkoutBase> WorkGet()
+        {
+            return WorkList;
+        }
+        public List<Machine> MachGet()
+        {
+            return MachList;
+        }
+        public List<Item> ItemGet()
+        {
+            return ItemList;
+        }
+    }
+}
